ff301e111933a79c3e4c5eb103e01a75679831e9
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-02-10 17:34:10 +0300

    add backend config

--- provider.tf
+++ provider.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-storage"
+    storage_account_name = "terraformstgaks99"
+    container_name       = "ubuntustate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
